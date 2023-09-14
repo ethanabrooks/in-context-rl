f38af511f16955af215fa43a5bb5e1c25247a3a2
Ethan Brooks <ethanabrooks@gmail.com>
2023-09-14 10:37:27 -0400

    use ragged indexing in optimal

--- src/data/optimal.py
+++ src/data/optimal.py
@@ -2,13 +2,4 @@ import data.full_history
 
 
 class Data(data.full_history.Data):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.unpadded_mask = self.unpadded_mask.cuda()
-        self.unpadded_data = self.unpadded_data.cuda()
-
-    def __getitem__(self, idx):
-        return self.unpadded_data[idx].flatten(), self.unpadded_mask[idx].flatten()
-
-    def __len__(self):
-        return len(self.unpadded_data)
+    pass
