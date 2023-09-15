09a368dc5c1e8a0753540314c65f73ec34f38805
Ethan Brooks <ethanabrooks@gmail.com>
2023-09-15 17:59:56 -0400

    add support for deep indexing in sweeps

--- src/main.py
+++ src/main.py
@@ -128,8 +128,18 @@ def sweep(  # dead: disable
         check_dirty()
 
     def trainable(sweep_params):
+        for k, v in sweep_params.items():
+            *path, key = k.split("/")
+            subconfig = config
+
+            for name in path:
+                subconfig = subconfig[name]
+            if key not in subconfig:
+                print(config)
+                raise ValueError(f"Failed to index into config with path {k}")
+            subconfig[key] = v
+
         sleep_time = 1
-        config.update(**sweep_params)
         while True:
             try:
                 run = setup_wandb(
