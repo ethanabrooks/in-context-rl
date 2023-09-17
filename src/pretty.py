9fc8dc4096d286fdbc61270b0640dda630828669
ethanabrooks <ethanabrooks@gmail.com>
2023-09-17 14:07:40 -0400

    use same width for all columns of row for simplicity

--- src/pretty.py
+++ src/pretty.py
@@ -11,26 +11,17 @@ def print_row(
     row: dict[str, Any],
     show_header: bool = True,
     formats: Optional[dict[str, Callable[[Any], str]]] = None,
-    widths: Optional[dict[str, float]] = None,
 ):
     row = dict(list(row.items())[:10])
     if formats is None:
         formats = {}
-    if widths is None:
-        widths = {}
-    for k, v in widths.items():
-        assert 0 < v < 1, f"Widths should be between 0 and 1, got {v} for {k}"
-    assert sum(widths.values()) <= 1, f"Sum of widths should be <= 1, got:\n{widths}"
 
     term_size = shutil.get_terminal_size((80, 20))
 
-    claimed = sum(widths.values())
-    remaining = 1 - claimed
-    default_width = remaining / (len(row) - len(widths))
-    widths = {k: widths.get(k, default_width) for k in row}
+    width = 1 / (len(row))
 
     def col_width(col: str):
-        return int(np.round(widths[col] * term_size.columns))
+        return int(np.round(width * term_size.columns))
 
     if show_header:
         header = [f"{k[:col_width(k) - 2]:<{col_width(k)}}" for k in row]
