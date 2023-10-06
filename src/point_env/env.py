fix optimal reward computation in PointEnv

src/point_env/env.py
@@ -51,8 +51,6 @@ class PointEnv(Env):
         self._task_space = spaces.Box(
             low=points.min(axis=0), high=points.max(axis=0), shape=(2,)
         )
-        self._optimal_rewards_iterator = None
-        self._optimal = None
 
     @property
     def action_space(self):
@@ -76,28 +74,11 @@ class PointEnv(Env):
     def get_task(self):
         return self._goal
 
-    def optimal_rewards_generator(
-        self,
-        start_state: np.ndarray,
-        goal: np.ndarray,
-    ):
-        current_state = start_state
-
-        while True:
-            optimal_action = goal - current_state
-            optimal_action = self.clip_action(optimal_action)
-            current_state = current_state + optimal_action
-            yield -np.linalg.norm(current_state - goal, ord=2)
-
     def reset(self):
         return self.reset_model()
 
     def reset_model(self):
         self._state = np.zeros(2)
-        self._optimal = []
-        self._optimal_rewards_iterator = self.optimal_rewards_generator(
-            self._state, self._goal
-        )
         return self._get_obs()
 
     def reset_task(self, task=None):
@@ -137,7 +118,5 @@ class PointEnv(Env):
         reward = self.reward(self._state, self._goal)
         done = False
         ob = self._get_obs()
-        optimal_reward = next(self._optimal_rewards_iterator)
-        self._optimal.append(optimal_reward)
-        info = {"task": self.get_task(), "optimal": self._optimal}
+        info = {"task": self.get_task(), "optimal": self.optimal}
         return ob, reward, done, info

src/point_env/learned.py
@@ -241,7 +241,11 @@ class Data(data.Data):
 
         # Split the rewards array at the end of each episode
         split_rewards = np.split(self.rewards, end_indices)
-        self.episode_rewards = [r for r in split_rewards if r.size > 0]
+        episode_rewards = [r for r in split_rewards if r.size > 0]
+        returns = [np.sum(r) for r in episode_rewards]
+        # Get the index of the episode with the best return
+        best_index = np.argmax(returns)
+        self.optimal = episode_rewards[best_index].flatten()
 
         masks = Step(
             tasks=make_mask(self.tasks),
@@ -344,21 +348,11 @@ class Data(data.Data):
         self,
         seed: int,
         use_heldout_tasks: bool,
-        include_optimal: bool = True,
         max_episode_steps: Optional[int] = None,
     ):
-        if include_optimal:
-            returns = [
-                np.sum(episode_rewards) for episode_rewards in self.episode_rewards
-            ]
-            # Get the index of the episode with the best return
-            best_index = np.argmax(returns)
-            optimal = self.episode_rewards[best_index].flatten()
-        else:
-            optimal = None
         env = PointEnv(
             goal_sampler="double-arc",
-            optimal=optimal,
+            optimal=self.optimal,
             seed=seed,
             test=use_heldout_tasks,
         )